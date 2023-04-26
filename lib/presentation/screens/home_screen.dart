3ca69574406e88fd6dfacadc65e74a4b817f97f3
applutter <tjrals604@naver.com>
2023-04-26 17:18:12 +0900

    Stream과 while 비교 테스트

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,11 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:personal_test/enum_routes.dart';
 import 'package:personal_test/presentation/blocs/user_info_cubit/user_info_cubit.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int number = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      print(timeStamp);
+      while (number < 10) {
+        await Future.delayed(Duration(seconds: 1));
+
+        number++;
+
+        print(number);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +39,7 @@ class HomeScreen extends StatelessWidget {
         child: TextButton(
           onPressed: () {
             final extra = context.read<UserInfoCubit>();
-            context.push(EnumRoutes.detail.toPath,extra: extra);
+            context.go(EnumRoutes.detail.toPath, extra: extra);
           },
           child: Text('버튼'),
         ),
